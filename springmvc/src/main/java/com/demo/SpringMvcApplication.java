0265aeff9db628dd383296282fe948f8e2383c29
oscar51011 <oscar51011@gmail.com>
2022-01-19 00:04:37 +0800

    springmvc project init

---
+++ springmvc/src/main/java/com/demo/SpringMvcApplication.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 實作 spring framework: spring-webmvc 模組
+ * @author oscar51011
+ * @date 2022年1月16日
+ */
+@SpringBootApplication
+public class SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcApplication.class, args);
+	}
+}
