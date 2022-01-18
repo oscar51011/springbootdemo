3fbbda5a7d28b559ad88f2e5339e4694a8fb9a57
oscar51011 <oscar51011@gmail.com>
2022-01-19 00:35:03 +0800

    refactor webMvc configuration location

--- springmvc/src/main/java/com/demo/config/WebMvcConfig.java
+++ springmvc/src/main/java/com/demo/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.demo.controller.config;
+package com.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
