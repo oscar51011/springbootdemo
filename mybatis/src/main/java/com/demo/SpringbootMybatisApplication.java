4c0c2419150999c46d212e88df03662f44d100ef
oscar51011 <oscar51011@gmail.com>
2022-04-09 18:04:44 +0800

    initialize mybatis module

---
+++ mybatis/src/main/java/com/demo/SpringbootMybatisApplication.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 實作 springboot 搭配 mybatis application
+ * @author oscar51011
+ * @date 2022年4月9日
+ */
+@SpringBootApplication
+public class SpringbootMybatisApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringbootMybatisApplication.class, args);
+	}
+
+}
