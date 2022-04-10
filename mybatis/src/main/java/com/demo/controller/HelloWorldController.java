866e989e033db5f23a30a24355e1adca041b6fc9
oscar51011 <oscar51011@gmail.com>
2022-04-10 09:03:32 +0800

    add mybatis test controller

---
+++ mybatis/src/main/java/com/demo/controller/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.service.HelloWorldService;
+
+/**
+ * 測試DAO功能運作
+ * @author oscar51011
+ * @date 2022年1月13日
+ */
+@RestController
+@RequestMapping("helloWorld")
+public class HelloWorldController {
+	
+
+	@Autowired
+	private HelloWorldService service;
+	
+	
+	@GetMapping("/test")
+	public String testDaoService() {
+		
+		service.testService();
+		
+		return "ok";
+	}
+}
